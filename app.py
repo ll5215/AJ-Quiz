230c309e3575b8c89202623a9a04b261ae46c91a
user03-test <foxlover17@gmail.com>
2024-07-04 00:52:40 +0900

    소연 풀 했습니다

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def get_questions(current_user):
     return jsonify({'success': True, 'questions': questions}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, port=3000)
